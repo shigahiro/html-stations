539ed9ae4595999f75f0beeac2754adec6a779ad
shigahiro <hiroki2131@icloud.com>
2026-01-17 19:33:42 +0900

    ステーション13クリア

--- src/scripts/station13.js
+++ src/scripts/station13.js
@@ -2,7 +2,13 @@ data = "global data";
 
 // このreturnDataに代入される関数の形をアロー関数に変えて"global data"が表示されるようにしてください
 // ↓関数の中の処理は変更しないでください
-const returnThisData = function () {
+// 通常の関数だとthisは呼び出し元のオブジェクトを参照する
+// const returnThisData = function () {
+//   return this.data;
+// };
+
+// アロー関数だとthisは定義された場所のスコープを参照します
+const returnThisData = () => {
   return this.data;
 };
 // ↑関数の中の処理は変更しないでください
